1b0e290ddc22a7f7b09d6b9b15017d512745ecf2
Owen Post <owen_post@sfu.ca>
2024-03-07 15:57:13 -0800

    added header and navigation to new pages

---
+++ src/Contribute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from '@aws-amplify/ui-react';
+import Header from './Header';
+
+const Contribute = () => {
+  return (
+    <View className="App">
+        <Header />
+            <div>
+            <h2>Contribute</h2>
+            <p>Submit new questions here:</p>
+      
+            </div>
+    </View>
+  );
+}
+
+export default Contribute;
